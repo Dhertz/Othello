b43ad2734a3d6e259a093d44da5e69e64bde3d8d
Owen Davies <tod11@doc.ic.ac.uk>
2012-11-26 12:45:24 +0000

    Work done on capturing, seems to work well

--- src/model/Board.java
+++ src/model/Board.java
@@ -1,13 +1,15 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Board {
-	
+
 	private Piece[][] board;
 	private static final int BOARDSIZE = 8;
-	
+
 	// These arrays allow us to loop through in all possible directions
-	private static final int[] DIRECT_X = { -1,  0,  1, -1, 1, -1, 0, 1 };
-    private static final int[] DIRECT_Y = { -1, -1, -1,  0, 0,  1, 1, 1 };
+	private static final int[] DIRECT_X = { -1, 0, 1, -1, 1, -1, 0, 1 };
+	private static final int[] DIRECT_Y = { -1, -1, -1, 0, 0, 1, 1, 1 };
 
 	public Board() {
 		board = new Piece[BOARDSIZE][BOARDSIZE];
@@ -20,66 +22,107 @@ public class Board {
 				board[i][j] = new Piece(PieceState.EMPTY);
 			}
 		}
-		
-		board[3][3].setState(PieceState.WHITE);
-		board[3][4].setState(PieceState.BLACK);
-		board[4][3].setState(PieceState.BLACK);
-		board[4][4].setState(PieceState.WHITE);
+
+		// Using Othello rules for initial setup, not Reversi
+		setPiece(4, 4, PieceState.WHITE);
+		setPiece(5, 4, PieceState.BLACK);
+		setPiece(4, 5, PieceState.BLACK);
+		setPiece(5, 5, PieceState.WHITE);
 	}
-	
+
 	public void setPiece(int column, int row, PieceState st) {
-		board[column - 1][row - 1].setState(st);
+		//System.out.println("setting (" + column + ", " + row + ") to " + st);
+		board[row - 1][column - 1].setState(st);
 	}
-	
-	public boolean isValidMove(int column, int row, PieceState st) {
-		int zeroIndexedColumn = column - 1;
-		int zeroIndexedRow = row - 1;
-		
-		// Bounds check and PieceState check
-		if (!inBounds(column, row) || getColour(zeroIndexedColumn, zeroIndexedRow) != PieceState.EMPTY) {
-			return false;
-		}
+
+	// Capture surrounding pieces - to be called after setPiece
+	public void capturePieces(int column, int row, PieceState st) {
+		ArrayList<Piece> captureList = new ArrayList<Piece>();
 		
 		for (int i = 0; i < DIRECT_X.length; i++) {
-            boolean sawOther = false, sawSelf = false;
-            int x = zeroIndexedColumn, y = zeroIndexedRow;
-            for (int j = 0; j < BOARDSIZE; j++) {
-                x += DIRECT_X[i];
-                y += DIRECT_Y[i];
+			int x = column, y = row;
 
-                if (!inBounds(x, y)) {
-                    break;
-                }
+			for (int j = 0; j < BOARDSIZE; j++) {
+				x += DIRECT_X[i];
+				y += DIRECT_Y[i];
 
-                PieceState foundState = getColour(x, y);
-                if (foundState == PieceState.EMPTY) {
-                    break;
-                } else if (foundState != st) {
-                    sawOther = true;
-                } else {
-                    sawSelf = true;
-                    break;
-                }
-            }
+				if (!inBounds(x, y)) {
+					break;
+				}
 
-            if (sawOther && sawSelf) {
-                return true;
-            }
-        }
-		
+				PieceState foundState = getColour(x, y);
+				if (foundState == PieceState.EMPTY) {
+					break;
+				} else if (foundState != st) {
+					captureList.add(getPiece(x, y));
+				} else {
+					for (Piece p : captureList) {
+						p.setState(st);
+					}
+					break;
+				}
+			}
+			captureList.clear();
+		}
+	}
+
+	public boolean isValidMove(int column, int row, PieceState st) {
+		// Bounds check and PieceState check
+		if (!inBounds(column, row)
+				|| getColour(column, row) != PieceState.EMPTY) {
+			return false;
+		}
+
+		// Loop through in all directions, checking for a piece of the opposite
+		// colour, break on other two cases
+		for (int i = 0; i < DIRECT_X.length; i++) {
+			boolean sawOther = false, sawSelf = false;
+			int x = column, y = row;
+
+			for (int j = 0; j < BOARDSIZE; j++) {
+				x += DIRECT_X[i];
+				y += DIRECT_Y[i];
+
+				if (!inBounds(x, y)) {
+					break;
+				}
+
+				PieceState foundState = getColour(x, y);
+				if (foundState == PieceState.EMPTY) {
+					break;
+				} else if (foundState != st) {
+					sawOther = true;
+				} else {
+					sawSelf = true;
+					break;
+				}
+			}
+
+			if (sawOther && sawSelf) {
+				return true;
+			}
+		}
+
 		return false;
 	}
-	
+
 	private PieceState getColour(int column, int row) {
-		return board[column][row].getState();
+		return board[row - 1][column - 1].getState();
 	}
 
 	private boolean inBounds(int column, int row) {
-		return column >= 0 && row >= 0 && column < BOARDSIZE && row < BOARDSIZE;
+		int zeroIndexedColumn = column - 1;
+		int zeroIndexedRow = row - 1;
+		return zeroIndexedColumn >= 0 && zeroIndexedRow >= 0
+				&& zeroIndexedColumn < BOARDSIZE && zeroIndexedRow < BOARDSIZE;
 	}
 
 	public Piece[][] getBoard() {
 		return board;
+	}
+
+	public Piece getPiece(int column, int row) {
+		return board[row - 1][column - 1];
 	}
 
 }
--- src/tests/ReversiTests.java
+++ src/tests/ReversiTests.java
@@ -8,29 +8,54 @@ import model.PieceState;
 import org.junit.Test;
 
 import view.ReversiOutput;
-import controller.ReversiController;
 
 public class ReversiTests {
 
 	@Test
-	public void testPrint() {
+	public void testSetPiece() {
 		Board gameBoard = new Board();
-		ReversiOutput printer = new ReversiOutput();
 		
-		gameBoard.setPiece(5, 6, PieceState.WHITE);
+		gameBoard.setPiece(3, 4, PieceState.BLACK);
 		
-		printer.printBoard(gameBoard);
+		assertEquals(PieceState.BLACK, gameBoard.getPiece(3, 4).getState());
 	}
 	
 	@Test
 	public void testValidMoves() {
 		Board gameBoard = new Board();
 		
+		assertTrue(gameBoard.isValidMove(3, 4, PieceState.BLACK));
 		assertTrue(gameBoard.isValidMove(4, 3, PieceState.BLACK));
 		assertTrue(gameBoard.isValidMove(5, 6, PieceState.BLACK));
 		assertFalse(gameBoard.isValidMove(3, 5, PieceState.BLACK));
 		assertFalse(gameBoard.isValidMove(10, 5, PieceState.BLACK));
 		assertFalse(gameBoard.isValidMove(4, 4, PieceState.BLACK));
 	}
-
+	
+	@Test
+	public void testCapture() {
+		Board gameBoard = new Board();
+		
+		gameBoard.setPiece(3, 4, PieceState.BLACK);
+		gameBoard.capturePieces(3, 4, PieceState.BLACK);
+		
+		assertEquals(PieceState.BLACK, gameBoard.getPiece(4, 4).getState());
+	}
+	
+	@Test
+	public void testCaptureSetSequence() {
+		Board gameBoard = new Board();
+		ReversiOutput printer = new ReversiOutput();
+		
+		gameBoard.setPiece(4, 3, PieceState.BLACK);
+		gameBoard.capturePieces(4, 3, PieceState.BLACK);
+		
+		printer.printBoard(gameBoard);
+		
+		gameBoard.setPiece(3, 5, PieceState.WHITE);
+		gameBoard.capturePieces(3, 5, PieceState.WHITE);
+		
+		printer.printBoard(gameBoard);
+	}
+	
 }
